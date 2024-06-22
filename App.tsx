de01e40ea652f01185ed3cba0682ba80a8f8ee22
Kayque Lucas <kayquedesigner@gmail.com>
2024-06-21 22:21:31 -0300

    feat: Perfil

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Perfil from './src/screens/perfil/index';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Perfil" component={Perfil} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/screens/perfil/index.tsx
@@ -0,0 +1,160 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
+import { createStackNavigator } from "@react-navigation/stack";
+// import Icon from "react-native-vector-icons/FontAwesome";
+import styles from "./styles";
+import axios from "axios";
+
+const Stack = createStackNavigator();
+
+const PerfilScreen: React.FC = () => {
+  const [usuario, setUsuario] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [senha, setSenha] = useState("");
+  const [novaSenha, setNovaSenha] = useState("");
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    axios.get("https://6675c1f4a8d2b4d072f15c00.mockapi.io/sarras/Usuarios/")
+      .then(res => {
+        const { usuario, email, username, senha } = res.data;
+        setUsuario(usuario);
+        setEmail(email);
+        setUsername(username);
+        setSenha(senha);
+    })
+  }, [])
+
+
+  const logout = async () => {
+    try {
+      const res = await axios.post( '', {
+        token: '',
+      });
+      if (res.status === 200) {
+        
+        Alert.alert("Deslogado com sucesso");
+        navigation.navigate("Home");
+      } else {
+        Alert.alert('Falha ao deslogar')
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert('');
+    }
+  };
+
+  const updatePassword = async () => {
+    try{
+    const res = await axios.post('', {
+      email: email,
+      senhaAntiga: senha,
+      novaSenha: novaSenha,
+    })
+    if (res.status === 200) {
+      Alert.alert('Senha atualizada!');
+    } else {
+      Alert.alert('Falha ao atualizar a senha');
+    }
+  } catch (error) {
+    console.error(error);
+    Alert.alert('');
+  }
+    alert("Senha atualizada!");
+  };
+
+  const criadores = () => {
+    alert("Navegar para a página de Criadores do App");
+  };
+
+  return (
+    <LinearGradient colors={["#1c1c1c", "#000"]} style={styles.container}>
+      {/* <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Icon name="arrow-left" size={24} color="#fff" />
+        </TouchableOpacity>
+        <Text style={styles.headerText}>Voltar</Text>
+      </View> */}
+
+      <View style={styles.profileContainer}>
+        <Text style={styles.title}>Perfil</Text>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Usuário</Text>
+          <TextInput
+            style={styles.input}
+            value={usuario}
+            onChangeText={setUsuario}
+            placeholderTextColor="#888"
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+            placeholderTextColor="#888"
+            keyboardType="email-address"
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Username</Text>
+          <TextInput
+            style={styles.input}
+            value={username}
+            onChangeText={setUsername}
+            placeholderTextColor="#888"
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Senha</Text>
+          <TextInput
+            style={styles.input}
+            value={senha}
+            onChangeText={setSenha}
+            secureTextEntry={true}
+            placeholderTextColor="#888"
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Nova Senha</Text>
+          <TextInput
+            style={styles.input}
+            value={novaSenha}
+            onChangeText={setNovaSenha}
+            secureTextEntry={true}
+            placeholder="Nova Senha"
+            placeholderTextColor="#888"
+          />
+          <Text style={styles.changePassword} onPress={updatePassword}>
+            alterar senha
+          </Text>
+        </View>
+        <TouchableOpacity style={styles.logoutButton} onPress={logout}>
+          <Text style={styles.logoutButtonText}>LogOut</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.creatorsButton} onPress={criadores}>
+        <Text style={styles.creatorsButtonText}>Criadores do App</Text>
+      </TouchableOpacity>
+      </View>
+
+    </LinearGradient>
+  );
+};
+
+const PerfilStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Perfil" component={PerfilScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default PerfilStack;
