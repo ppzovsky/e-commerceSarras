144fc09a0e03ba9a91385493065cc4439a221f7b
Bianco <arthnilo@gmail.com>
2024-06-24 19:11:43 -0300

    feat: splashScreen e context (correção)

--- src/components/useContext/userProfile.tsx
+++ src/components/useContext/userProfile.tsx
@@ -1,29 +1,23 @@
 import React, { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
-// Cria o contexto
-export const UserContext = createContext();
 
-// Cria o Provider
-export const UserProvider = ({ children }) => {
+export const UserContext = createContext( { usuario: '' } );
+
+
+export const UserProvider = ({ children }: any) => {
   const [usuario, setUsuario] = useState('');
-  const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
-  const [senha, setSenha] = useState('');
 
   useEffect(() => {
     axios.get("https://6675c1f4a8d2b4d072f15c00.mockapi.io/sarras/Usuarios/")
       .then(res => {
-        const { usuario, email, username, senha } = res.data;
+        const { usuario } = res.data;
         setUsuario(usuario);
-        setEmail(email);
-        setUsername(username);
-        setSenha(senha);
       })
   }, []);
 
   return (
-    <UserContext.Provider value={{ usuario, email, username, senha }}>
+    <UserContext.Provider value={{ usuario }}>
       {children}
     </UserContext.Provider>
   );
